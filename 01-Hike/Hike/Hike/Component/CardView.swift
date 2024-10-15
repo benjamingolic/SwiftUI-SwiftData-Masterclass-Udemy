6a94356783851e1839118b4f1afef93de184dd9d
benjamingolic <golicbenjamin.school@gmail.com>
2024-10-15 21:43:06 +0200

    Motion Animation

--- 01-Hike/Hike/Hike/Component/CardView.swift
+++ 01-Hike/Hike/Hike/Component/CardView.swift
@@ -56,15 +56,7 @@ struct CardView: View {
         .padding(.horizontal, 30)
         
         ZStack {
-          Circle()
-            .fill(
-              LinearGradient(
-                colors: [.colorIndigoMedium, .colorSalmonLight],
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-              )
-            )
-            .frame(width: 256, height: 256)
+          CustomCircleView()
           
           Image("image-\(imageNumber)")
             .resizable()
---
+++ 01-Hike/Hike/Hike/Component/CustomCircleView.swift
@@ -0,0 +1,37 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by Benjamin Golic on 15.10.24.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+  @State private var isAnimated: Bool = false
+  
+  var body: some View {
+    ZStack {
+      Circle()
+        .fill(
+          LinearGradient(
+            colors: [.colorIndigoMedium, .colorSalmonLight],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+          )
+        )
+        .onAppear {
+          withAnimation(.linear(duration: 3.0).repeatForever(autoreverses: true)) {
+            isAnimated.toggle()
+          }
+        }
+      
+      MotionAnimationView()
+    }
+    .frame(width: 256, height: 256)
+  }
+}
+
+#Preview {
+  CustomCircleView()
+}
