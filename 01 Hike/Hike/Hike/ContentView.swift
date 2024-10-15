dd66695808708574266a046db84a659c012dd522
benjamingolic <golicbenjamin.school@gmail.com>
2024-10-15 20:27:35 +0200

    Project Setup

---
+++ 01 Hike/Hike/Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Benjamin Golic on 15.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      Image("image-1")
+        .resizable()
+        .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 01 Hike/Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Benjamin Golic on 15.10.24.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
