fc0ab98bf11015faf0b5cb50b408087e97259471
benjamingolic <golicbenjamin.school@gmail.com>
2024-10-19 19:40:12 +0200

    Pinch: now added Drag Gesture, last commit was DoubleTabGesture

--- 03-Pinch/Pinch/Pinch/Screen/ContentView.swift
+++ 03-Pinch/Pinch/Pinch/Screen/ContentView.swift
@@ -10,6 +10,14 @@ import SwiftUI
 struct ContentView: View {
   @State private var isAnimating: Bool = false
   @State private var imageScale: CGFloat = 1
+  @State private var imageOffset: CGSize = .zero // CGSize(width: 0, height: 0)
+  
+  func resetImageState() {
+    return withAnimation(.spring()) {
+      imageScale = 1
+      imageOffset = .zero
+    }
+  }
   
   var body: some View {
     NavigationStack {
@@ -19,8 +27,9 @@ struct ContentView: View {
           .aspectRatio(contentMode: .fit)
           .cornerRadius(10)
           .padding()
-          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y:2 )
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1 : 0)
+          .offset(x: imageOffset.width, y: imageOffset.height)
           .animation(.linear(duration: 0.5), value: isAnimating)
           .scaleEffect(imageScale)
           .onTapGesture(count: 2, perform: {
@@ -29,11 +38,22 @@ struct ContentView: View {
                 imageScale = 5
               }
             } else {
-              withAnimation(.spring()) {
-                imageScale = 1
-              }
+              resetImageState()
             }
           })
+          .gesture(
+            DragGesture()
+              .onChanged { value in
+                withAnimation(.linear(duration: 1)) {
+                  imageOffset = value.translation
+                }
+              }
+              .onEnded { _ in
+                if imageScale <= 1 {
+                  resetImageState()
+                }
+              }
+          )
       }
       .navigationTitle("Pinch & Zoom")
       .navigationBarTitleDisplayMode(.inline)
