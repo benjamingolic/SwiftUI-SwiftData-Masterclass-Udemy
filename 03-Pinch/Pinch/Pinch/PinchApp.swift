c60fe3cf0d7c68e34ee0c6b6fd816bbea784bee4
benjamingolic <golicbenjamin.school@gmail.com>
2024-10-19 19:11:52 +0200

    Pinch-ProjectSetup

---
+++ 03-Pinch/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Benjamin Golic on 19.10.24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
